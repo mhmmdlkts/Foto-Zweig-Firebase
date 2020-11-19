2982bcc6c5ece9c56589da4684f2709ce2b78e8d
Muhammed Ali Aktaş <mhmmdlkts@gmail.com>
2020-11-19 20:46:14 +0100

    Upload image function added

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,18 @@
+// firebase-multi use foto firebase deploy
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const db = admin.database();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.upload = functions.region('europe-west1').https.onRequest(uploadImage);
+
+async function uploadImage (request, response)  {
+    const foto = JSON.parse(decodeURI(request.query.foto));
+    foto.id = getRandomInt(1000);
+    await db.ref("fotos").push(foto);
+    response.status(200).send({id:foto.id});
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
